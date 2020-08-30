5520e8ad87f70adbe5481d459b38eae506666150
lidianxiang <ldx2703@gmail.com>
2020-08-30 23:08:07 +0800

    hello world

---
+++ hello_world.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+    
+//     cout << "hello, world!" << endl;
+//     // system("pause");
+//     return 0;
+
+// }
+
+#include<stdio.h>
+int main(){
+    printf("Hello World");
+    return 0;
+}
