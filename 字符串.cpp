86894df9967b888f2d18bcd92088e86513130f3b
lidianxiang <ldx2703@gmail.com>
2020-08-31 09:47:53 +0800

    字符串

---
+++ 字符串.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    // C风格字符串
+    char str[] = "hello world";
+    cout << str << endl;
+
+    // C++风格的字符串
+    string str2 = "hello world";
+    cout << str2 << endl;
+
+    return 0;
+}
