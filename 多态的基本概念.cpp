357e5f087ada939a0066c5addc0a1c023b0e4d21
lidianxiang <ldx2703@gmail.com>
2020-09-09 09:27:17 +0800

    多态的基本概念

---
+++ 多态的基本概念.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+/*
+
+多态分为两类：
+    1、静态多态： 函数重载和运算符重载属于静态多态，复用函数名
+    2、多态多态： 派生类和虚函数实现运行时多态
+
+静态多态和多态多态的区别：
+    1、静态多态的函数地址早绑定 - 编译阶段确定函数地址
+    2、动态多态的函数地址晚绑定 - 运行阶段确定函数地址
+
+*/
+
+// 动物类
+class Animal{
+public:
+    // 加上virtual后，地址就会晚绑定，在运行阶段确定函数地址
+    virtual void speak(){
+        cout << "动物在说话" << endl;
+    }
+};
+
+class Cat: public Animal{
+public:
+    void speak(){
+        cout << "小猫在说话" << endl;
+    };
+};
+
+class Dog: public Animal{
+public:
+    void speak(){
+        cout << "小狗在说话" << endl;
+    };
+};
+
+void doSpeak(Animal &animal){
+    // 地址早绑定，所以输出的是父类的speak函数
+    animal.speak();
+};
+
+void test01(){
+    Cat cat;
+    doSpeak(cat);
+
+    Dog dog;
+    doSpeak(dog);
+};
+
+
+int main(){
+
+    test01();
+
+    return 0;
+
+}
