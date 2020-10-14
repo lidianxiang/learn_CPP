8de1e437fe00176bacf0137997348b9d09fb8c1d
lidianxiang <ldx2703@gmail.com>
2020-10-14 09:29:31 +0800

    机房预约系统

---
+++ 机房预约系统/orderFile.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<iostream>
+using namespace std;
+#include<map>
+#include "globalFile.h"
+#include<fstream>
+
+class OrderFile
+{
+public:
+    // 构造函数
+    OrderFile();
+
+    // 更新预约记录
+    void updateOrder();
+
+    // 记录的容器
+    map<int, map<string, string> > m_orderData;
+
+    // 预约记录条数
+    int m_Size;
+
+};
